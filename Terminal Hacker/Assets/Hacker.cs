7b530506ed6e7a12628997d06039b394f56d0415
sourabh-swargam <swargam.sourabh@gmail.com>
2020-09-07 15:45:29 +0530

    completed

--- Terminal Hacker/Assets/Hacker.cs
+++ Terminal Hacker/Assets/Hacker.cs
@@ -6,9 +6,15 @@ using UnityEngine;
 public class Hacker : MonoBehaviour
 {
 
+    // Game configuration data
+    const string menuHint = "You many type menu at any time.";
+    string[] level1Passwords = {"books", "aisle", "shelf", "password", "font", "borrow"};
+    string[] level2Passwords = {"prisoner", "handcuffs", "holster", "uniform", "arrest"};
+    string[] level3Passwords = { "starfield", "telescope", "environment", "exploration", "astronauts" };
+
     // Game State
     int level;
-
+    string password;
     enum Screen { MainMenu, Password, Win};
     Screen currentScreen;
 
@@ -24,7 +30,8 @@ public class Hacker : MonoBehaviour
         Terminal.ClearScreen();
         Terminal.WriteLine("What would you like to hack into?\n");
         Terminal.WriteLine("Press 1 for the local library.");
-        Terminal.WriteLine("Press 2 for the police station.\n");
+        Terminal.WriteLine("Press 2 for the police station.");
+        Terminal.WriteLine("Press 3 for NASA!\n");
         Terminal.WriteLine("Enter your selection:");
     }
 
@@ -34,38 +41,124 @@ public class Hacker : MonoBehaviour
         {
             ShowMainMenu();
         }
+        else if (input == "quit" || input == "close" || input == "exit")
+        {
+            Application.Quit();
+        } 
         else if (currentScreen == Screen.MainMenu)
         {
             RunMainMenu(input);
+        }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
         }
     }
 
     private void RunMainMenu(string input)
     {
-        if (input == "1")
+        bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
+        if (isValidLevelNumber)
         {
-            level = 1;
-            StartGame();
-        }
-        else if (input == "2")
-        {
-            level = 2;
-            StartGame();
+            level = int.Parse(input);
+            AskForPassword();
         }
         else if (input == "007")
         {
+            Terminal.WriteLine(menuHint);
             Terminal.WriteLine("Please choose a level Mr Bond!");
         }
         else
         {
+            Terminal.WriteLine(menuHint);
             Terminal.WriteLine("Please choose a valid level.");
         }
     }
 
-    void StartGame()
-     {
+    void AskForPassword()
+    {
         currentScreen = Screen.Password;
+        Terminal.ClearScreen();
+        SetRandomPassword();
+        Terminal.WriteLine(menuHint);
         Terminal.WriteLine("You have chosen level " + level);
-        Terminal.WriteLine("Please enter your password: ");
+        Terminal.WriteLine("Enter your password, hint: " + password.Anagram());
+    }
+
+    void SetRandomPassword()
+    {
+        switch (level)
+        {
+            case 1:
+                password = level1Passwords[UnityEngine.Random.Range(0, level1Passwords.Length)];
+                break;
+            case 2:
+                password = level2Passwords[UnityEngine.Random.Range(0, level2Passwords.Length)];
+                break;
+            case 3:
+                password = level3Passwords[UnityEngine.Random.Range(0, level3Passwords.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid level number.");
+                break;
+        }
+    }
+
+    void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            DisplayWinScreen();
+        }
+        else
+        {
+            AskForPassword();
+        }
+    }
+
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+    }
+
+    void ShowLevelReward()
+    {
+        Terminal.WriteLine(menuHint);
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine("Have a book...");
+                Terminal.WriteLine(@"
+    _______
+   /      //
+  /      //
+ /______//      
+(______(/
+");
+                break;
+            case 2:
+                Terminal.WriteLine("You got the prison key.");
+                Terminal.WriteLine(@"
+ __
+/0 \_______
+\__/-=' = '         
+");
+                break;
+            case 3:
+                Terminal.WriteLine(@"
+ _ __   __ _ ___  __ _
+| '_ \ / _` / __|/ _` |
+| | | | (_| \__ \ (_| |
+|_| |_|\__,_|___)\__,_|
+"
+                );
+                Terminal.WriteLine("Welcome to NASA's internal system!");
+                break;
+            default:
+                Debug.LogError("Invalid level reached");
+                break;
+        }
     }
 }
