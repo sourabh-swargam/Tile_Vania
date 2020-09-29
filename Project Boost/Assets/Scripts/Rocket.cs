e6ce70c2ee894d538ed7d9b3453e38249902e869
sourabh-swargam <swargam.sourabh@gmail.com>
2020-09-29 19:32:19 +0530

    added audio source

--- Project Boost/Assets/Scripts/Rocket.cs
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -7,10 +7,12 @@ public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,6 +26,10 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
 
         if (Input.GetKey(KeyCode.A))
